377f5d3e15468bdf448c4a37bce5861ca6b4131c
Erik Henning <henningerik2@gmail.com>
2025-12-28 17:38:31 -0300

    Conn postgres prd

--- commerce_new/connections/all_connections.py
+++ commerce_new/connections/all_connections.py
@@ -21,3 +21,26 @@ def postgres_local():
     except Exception as e:
         print(f"Erro ao conectar: {e}")
         return None
+
+
+def postgres_prd():
+    host = "35.247.250.163"
+    port = 5432
+    database = "powder_intelligence"
+    user = "postgres"
+    password = "vS@L_iAUgHJa^[8-"
+
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=database,
+            user=user,
+            password=password
+        )
+        print("Conexão estabelecida com sucesso!")
+        return conn
+
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
+        return None
